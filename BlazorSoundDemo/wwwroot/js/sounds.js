82097ce6b4810ac956ff6e980a260546b0e6effb
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-02-18 12:48:24 -0700

    Add project files.

---
+++ BlazorSoundDemo/wwwroot/js/sounds.js
@@ -0,0 +1,7 @@
+﻿window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+}
+
+window.PauseAudio = (elementName) => {
+    document.getElementById(elementName).pause();
+}
